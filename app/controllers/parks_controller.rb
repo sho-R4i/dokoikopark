52ea41311c910a1d9edaeee0b4b4b65c394a666c
ShoNakanishi <165697131+sho-R4i@users.noreply.github.com>
2024-06-26 13:13:05 +0000

    [Add]parks_controller,users_controllerの設定

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,60 @@
+class ParksController < ApplicationController
+  def new
+    @park = Park.new
+  end
+  
+  def create
+    @park = Parl.new(park_params)
+    @park.user_id = current_user.id
+    if @park.save
+      flash[:notice] = '公園の情報を投稿しました。'
+      redirect_to park_path(@park)
+    else
+      @parks = Park.all
+      render :index
+    end
+  end
+  
+  def index
+    @parks = Park.all
+  end
+  
+  def show
+    @park = Park.find(params[:id])
+    @user = User.find(@parks.user.id)
+  end
+  
+  def edit
+    is_matching_login_user
+    @park = Park.find(params[:id])
+  end
+  
+  def update
+    is_matching_login_user
+    @park = Park.find(params[:id])
+    if @park.update(park_params)
+      flash[:notice] = '公園の情報を更新しました。'
+      redirect_to park_path
+    else
+      render :edit
+    end
+  end
+  
+  
+  def destroy
+    @park = Park.find(params[:id])
+    if @park.destroy
+      flash[:notice] = '公園の情報を削除しました。'
+      redirect_to parks_path
+    else
+    end
+  end
+  
+  def is_matching_login_user
+    park = Park.find(params[:id])
+    unless park.user.id == current_user.id
+      redirect_to parks_path
+    end
+  end
+  
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
+  def show
+    @user = User.find(params[:id])
+    @parks = @user.parks
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'ユーザー情報を更新しました。'
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+  
 end
