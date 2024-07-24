bd8932359152f7eb59e1eda1827cbd162f8a66e5
EC2 Default User <ec2-user@ip-172-31-39-107.ap-northeast-1.compute.internal>
2024-07-24 14:13:36 +0000

    [Update]管理者ページの実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def top
+    redirect_to parks_path if current_user
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :admin do
+    get 'comments/index'
+  end
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
@@ -10,7 +16,8 @@ Rails.application.routes.draw do
   
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy]
+    resources :users, only: [:index, :destroy]
+    resources :comments, only: [:index, :destroy]
   end
 
   root to: 'homes#top'
